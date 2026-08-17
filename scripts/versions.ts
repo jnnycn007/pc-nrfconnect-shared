64496f18e0453b8fd7b7ea0418f382c23efb1959
Marko Schulz <Marko.Schulz@nordicsemi.no>
2026-08-17 13:39:28 +0200

    Update Electron to 43

--- scripts/versions.ts
+++ scripts/versions.ts
@@ -4,9 +4,9 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-export const electron = '32.1.2';
-export const chrome = '128.0.6613.162';
-export const node = '20.17.0';
+export const electron = '43.4.0';
+export const chrome = '150.0.7871.224';
+export const node = '24.18.1';
 
 // Chrome from Electron 32, used in launcher >=5.1.0, <5.4.0
 export const chromeToBuildFor = '128.0.6613';
